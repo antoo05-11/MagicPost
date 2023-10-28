811dfc79ac0b46f48ed5e1085bacbf326ae94c8b
thanhan <antooo5113@gmail.com>
2023-10-29 02:30:19 +0700

    Add sample find distance between two addresses.

--- server/src/api/controllers/address.js
+++ server/src/api/controllers/address.js
@@ -9,6 +9,11 @@ Address.belongsTo(District, { foreignKey: 'districtID' });
 Address.belongsTo(Province, { foreignKey: 'provinceID' });
 District.belongsTo(Province, { foreignKey: 'provinceID' });
 
+
+const request = require('request');
+const parseString = require('xml2js').parseString;
+
+
 export const getAddressByID = async (req, res) => {
     Address.findOne({
         where: { addressID: req.params.id },
@@ -30,12 +35,35 @@ export const getAddressByID = async (req, res) => {
         .then((result) => {
             if (result) {
                 const response = {
-                    province: result.district.province,
+                    province: result.district.province.name,
                     district: result.district.name,
                     commune: result.commune.name,
                     detail: result.dataValues.detail,
                     exactPostion: result.dataValues.exactPosition
                 }
+
+                const origin = response.detail + ',' + response.commune + ','
+                    + response.district + ',' + response.province + ',VietNam';
+                const destination = 'Số  3 Đường Xuân Thủy' + ',' + response.commune + ','
+                + response.district + ',' + response.province + ',VietNam';
+
+                const url = encodeURI(`http://dev.virtualearth.net/REST/V1/Routes/Driving?o=xml&wp.0=${origin}&wp.1=${destination}&key=${process.env.BING_MAP_API_KEY}`);
+                console.log(url);
+                request(url, (error, response, body) => {
+                    if (!error && response.statusCode == 200) {
+                        parseString(body, (err, result) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                            const travelDistance = result.Response.ResourceSets[0].ResourceSet[0].Resources[0].Route[0].TravelDistance[0];
+
+                            console.log('TravelDistance:', travelDistance);
+                        });
+                    } else {
+                        console.log('API calling error: ', error);
+                    }
+                });
                 res.json(response);
 
             } else {
@@ -45,4 +73,8 @@ export const getAddressByID = async (req, res) => {
         .catch((error) => {
             console.error('Error:', error);
         });
+}
+
+export const addNewAddress = async(req, res) => {
+
 }
