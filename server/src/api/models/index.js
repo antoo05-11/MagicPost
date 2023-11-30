4866ba6ca2d06b2cfc943c9f96bbdc5dc49e3d87
antoo05-11 <antooo5113@gmail.com>
2023-11-30 21:17:41 +0700

    Change database URI for sequelize config.

--- server/src/api/models/index.js
+++ server/src/api/models/index.js
@@ -1,14 +1,18 @@
 import { DataTypes, Sequelize } from "sequelize";
-const dbconfig = require("../config/dbconfig.js").localhost;
+require('dotenv').config()
+
+const sequelize = new Sequelize(
+    process.env.DB_URI, {}
+)
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connected to Database server.");
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+    .authenticate()
+    .then(() => {
+        console.log("Connected to Database server.");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 
 const db = {};
 db.Sequelize = Sequelize;
@@ -21,24 +25,15 @@ db.addresses = require("./routing_point/address.js")(sequelize, DataTypes);
 db.routes = require("./routing_point/route.js")(sequelize, DataTypes);
 db.employees = require("./human/employee.js")(sequelize, DataTypes);
 db.customers = require("./human/customer.js")(sequelize, DataTypes);
-db.goods_points = require("./routing_point/goods_point.js")(
-  sequelize,
-  DataTypes
-);
-db.transaction_points = require("./routing_point/transaction_point.js")(
-  sequelize,
-  DataTypes
-);
+db.goods_points = require("./routing_point/goods_point.js")(sequelize, DataTypes);
+db.transaction_points = require("./routing_point/transaction_point.js")(sequelize, DataTypes);
 db.orders = require("./order/order.js")(sequelize, DataTypes);
 db.processes = require("./order/process.js")(sequelize, DataTypes);
 db.goods = require("./order/goods.js")(sequelize, DataTypes);
 db.employee_roles = require("./human/employeeRole.js")(sequelize, DataTypes);
-db.routing_points = require("./routing_point/routing_point.js")(
-  sequelize,
-  DataTypes
-);
+db.routing_points = require("./routing_point/routing_point.js")(sequelize, DataTypes);
 
 db.sequelize.sync({ force: false }).then(() => {
-  console.log("Re-sync are done!");
+    console.log("Re-sync are done!");
 });
 module.exports = db;
