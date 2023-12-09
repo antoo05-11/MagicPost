9418d60163a3e06380e97a93f3bd4cc406df7b83
LockyYud <dominhduybk7@gmail.com>
2023-12-09 23:09:53 +0700

    add color

--- client/src/app/employees/page.js
+++ client/src/app/employees/page.js
@@ -1,18 +1,10 @@
 "use client";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import OrderTable from "@/components/employee/order-table";
-import useSWR from "swr";
-import { getEmployee } from "@/api/data";
 import { Container, Row, Col } from "react-bootstrap";
-import Card from "@/components/dashboard/cards";
-import DemoChart from "@/components/Chart/Chart";
-import { useParams, usePathname } from "next/navigation";
-import dynamic from "next/dynamic";
 import Overview from "@/components/Overview/Overview";
 import Statistic from "@/components/Statistic/Statistic";
 import StatisticGoodsPoint from "@/components/StatisticGoodsPoint/StatisticGoodsPoint";
 import StatisticTransPoint from "@/components/StatisticTransPoint/StatisticTransPoint";
-
+import { motion } from "framer-motion";
 export const orderDetails = [
   "nguoi gui",
   "nguoi nhan",
@@ -22,18 +14,11 @@ export const orderDetails = [
 ];
 export default function AdminPage() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {/* <div id="dashboard">
-        <Card title={"Tong don hang"} value={10} />
-        <Card title={"Tong don hang"} value={10} />
-        <Card title={"Tong don hang"} value={10} />
-        <Card title={"Tong don hang"} value={10} />
-      </div> */}
-      {/* <DemoChart /> */}
-      <Row >
+    <motion.div layout>
+      <motion.h1 layout>Dashboard</motion.h1>
+      <Row>
         <Col xs={12}>
-          <Statistic userRole={'leader'} />
+          <Statistic userRole={"leader"} />
         </Col>
       </Row>
 
@@ -49,10 +34,7 @@ export default function AdminPage() {
         <Col xs={12} md={4}>
           <StatisticTransPoint />
         </Col>
-
-
       </Row>
-
-    </div>
+    </motion.div>
   );
 }
