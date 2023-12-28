3ce07d6dfa1947ed5625260379302e09e270d381
LockyYud <dominhduybk7@gmail.com>
2023-12-29 05:25:31 +0700

    fix update order

--- client/src/api/utils.js
+++ client/src/api/utils.js
@@ -124,18 +124,34 @@ export const listUrl = {
 };
 
 export const orderStatus = {
-  forwarded: { now: "Đã vận chuyển", next: null, color: "success" },
+  forwarded: {
+    name: "forwarded",
+    now: "Đã vận chuyển",
+    next: null,
+    color: "success",
+  },
   arriving: {
+    name: "arriving",
     now: "Đang vận chuyển đến",
     next: "Xác nhận đã đến",
     color: "warning",
   },
-  on_stock: { now: "Trong kho", next: "Xác nhận chuyển tiếp", color: "danger" },
-  customer_sent: {
-    now: "Người nhận đã nhận được hàng",
-    next: "Người nhận đã nhận được hàng",
+  on_stock: {
+    name: "on_stock",
+    now: "Trong kho",
+    next: "Xác nhận chuyển tiếp",
+    color: "danger",
   },
-  customer_returned: { now: "Hàng bị hoàn trả", next: "Hàng bị hoàn trả" },
+  customer_sent: {
+    name: "customer_sent",
+    now: "Người nhận đã nhận được hàng",
+    next: "Xác nhận người nhận đã nhận được hàng",
+  },
+  customer_returned: {
+    name: "customer_returned",
+    now: "Hàng bị hoàn trả",
+    next: "Xác nhận hàng bị hoàn trả",
+  },
 };
 
 export const goodStatus = {
